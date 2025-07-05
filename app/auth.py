244d17ef6382748ca7947c541c75ca2829718997
Daniela ESTUPINAN MOSCOSO <estupinand@MD-ESTUPINAND-MC-2.local>
2025-07-04 20:05:15 -0500

    Edit python files

--- app/auth.py
+++ app/auth.py
@@ -1,17 +1,12 @@
 import jwt
 import os
 from dotenv import load_dotenv
-
 load_dotenv() 
-
 API_KEY = os.getenv("API_KEY")
 JWT_SECRET = os.getenv("JWT_SECRET")
 
-
-
 def check_api_key(req):
     return req.headers.get("X-Parse-REST-API-Key") == API_KEY
-
 
 def check_jwt(req):
     token = req.headers.get("X-JWT-KWY")
@@ -21,6 +16,5 @@ def check_jwt(req):
     except Exception:
         return False
 
-
 def generate_jwt():
-    return jwt.encode({"user": "devops"}, JWT_SECRET, algorithm="HS256")
+    return jwt.encode({"user": "devops"}, JWT_SECRET, algorithm="HS256") 
