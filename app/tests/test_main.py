0507e7236f5538eea038eb641856343d14d75661
Daniela ESTUPINAN MOSCOSO <estupinand@MD-ESTUPINAND-MC-2.local>
2025-07-04 20:15:15 -0500

    Edit python files

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,32 +1,21 @@
-import sys
-import os
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))  # noqa: E402
-
 from app.main import app
 from app.auth import generate_jwt
 
 client = app.test_client()
 headers = {
-    "X-Parse-REST-API-Key": (
-        "2f5ae96c-b558-4c7b-a590-a501ae1c3f6c"
-    ),
+    "X-Parse-REST-API-Key": "2f5ae96c-b558-4c7b-a590-a501ae1c3f6c",
     "X-JWT-KWY": generate_jwt(),
-    "Content-Type": "application/json",
+    "Content-Type": "application/json"
 }
 
 
 def test_valid_post():
-    response = client.post(
-        "/DevOps",
-        json={
-            "message": "This is a test",
-            "to": "Juan Perez",
-            "from": "Rita Asturia",
-            "timeToLifeSec": 45,
-        },
-        headers=headers,
-    )
+    response = client.post("/DevOps", json={
+        "message": "This is a test",
+        "to": "Juan Perez",
+        "from": "Rita Asturia",
+        "timeToLifeSec": 45
+    }, headers=headers)
     assert response.status_code == 200
     assert "Juan Perez" in response.get_json()["message"]
 
