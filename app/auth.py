a74db21beec89ef80ee705a2177a860b8d12f0ea
Daniela ESTUPINAN MOSCOSO <estupinand@MD-ESTUPINAND-MC-2.local>
2025-07-04 20:04:09 -0500

    Edit python files

--- app/auth.py
+++ app/auth.py
@@ -1,7 +1,12 @@
 import jwt
+import os
+from dotenv import load_dotenv
 
-API_KEY = "2f5ae96c-b558-4c7b-a590-a501ae1c3f6c"
-JWT_SECRET = "supersecretkey"
+load_dotenv() 
+
+API_KEY = os.getenv("API_KEY")
+JWT_SECRET = os.getenv("JWT_SECRET")
+
 
 
 def check_api_key(req):
--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,37 +1,26 @@
-import unittest
 from app.main import app
 from app.auth import generate_jwt
 
-
-class DevOpsTestCase(unittest.TestCase):
-
-    def setUp(self):
-        self.client = app.test_client()
-        self.headers = {
-            "X-Parse-REST-API-Key": "2f5ae96c-b558-4c7b-a590-a501ae1c3f6c",
-            "X-JWT-KWY": generate_jwt(),
-            "Content-Type": "application/json"
-        }
-
-    def test_valid_post(self):
-        response = self.client.post(
-            "/DevOps",
-            json={
-                "message": "This is a test",
-                "to": "Juan Perez",
-                "from": "Rita Asturia",
-                "timeToLifeSec": 45,
-            },
-            headers=self.headers,
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertIn("Juan Perez", response.get_json()["message"])
-
-    def test_invalid_method(self):
-        response = self.client.get("/DevOps")
-        self.assertEqual(response.status_code, 405)
-        self.assertEqual(response.data, b"ERROR")
+client = app.test_client()
+headers = {
+    "X-Parse-REST-API-Key": "2f5ae96c-b558-4c7b-a590-a501ae1c3f6c",
+    "X-JWT-KWY": generate_jwt(),
+    "Content-Type": "application/json"
+}
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_valid_post():
+    response = client.post("/DevOps", json={
+        "message": "This is a test",
+        "to": "Juan Perez",
+        "from": "Rita Asturia",
+        "timeToLifeSec": 45
+    }, headers=headers)
+    assert response.status_code == 200
+    assert "Juan Perez" in response.get_json()["message"]
+
+
+def test_invalid_method():
+    response = client.get("/DevOps")
+    assert response.status_code == 405
+    assert response.data == b"ERROR"
